7cbe4c43ff78fa4945c3def1584090da06caa692
tanonibrenda <brendayohenatanoni@gmail.com>
2024-11-28 23:02:22 -0300

    contact2

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
+// Requerir dotenv para manejar variables de entorno
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');  // Importar cors
 const userRoutes = require('./routes/userRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 const listRoutes = require('./routes/listRoutes');
-const contactRoutes = require('./routes/contactRoutes')
-require('./db/databaseSetup')
+const contactRoutes = require('./routes/contactRoutes');
+require('./db/databaseSetup');
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./db/database.db');  // Asegúrate de que la ruta es correcta
@@ -23,7 +26,7 @@ app.get('/', (req, res) => {
 app.use('/api/users', userRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/lists', listRoutes);
-app.use('/api/contact',)
+app.use('/api/contact', contactRoutes); // Usar las rutas de contacto correctamente
 
 // Ruta temporal para verificar los datos de usuarios
 app.get('/verify-users', (req, res) => {
