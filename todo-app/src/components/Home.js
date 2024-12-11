0e49430c14db9d0f17348d2b81dfde2bb168cc67
tanonibrenda <brendayohenatanoni@gmail.com>
2024-12-11 09:35:13 -0300

    bugClass

--- todo-app/src/components/Home.js
+++ todo-app/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => (
   <main>
     <section className="py-5">
-    <div class="container px-4 px-lg-5 my-5">
+    <div className="container px-4 px-lg-5 my-5">
       <div className="container">
         <div className="row align-items-center">
           {/* Columna izquierda con la imagen */}
