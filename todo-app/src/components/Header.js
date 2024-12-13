82d94f90242368c4f572bb50cfdadeb9247b22b8
tanonibrenda <brendayohenatanoni@gmail.com>
2024-12-13 02:25:57 -0300

    burger

--- todo-app/src/components/Header.js
+++ todo-app/src/components/Header.js
@@ -23,8 +23,19 @@ const Header = () => {
       <nav className={`navbar navbar-expand-lg ${darkMode ? 'dark-header' : ''}`}>
         <div className="container-fluid">
           <Link to="/" className="navbar-brand">
-      <img src="/img/checkverde.gif" alt="Logo para ir a pagina principal   " style={{ height: '40px', marginRight: '10px' }} />
+            <img src="/img/checkverde.gif" alt="Logo para ir a pagina principal" style={{ height: '40px', marginRight: '10px' }} />
           </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
