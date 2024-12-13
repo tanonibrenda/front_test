9b2ad790b53074b5fd86846adf46a9eca0084e31
tanonibrenda <brendayohenatanoni@gmail.com>
2024-12-13 02:28:46 -0300

    burgerBoot

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const container = document.getElementById('root');
 console.log('index.js - Container:', container);
